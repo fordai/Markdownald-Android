4fe9ea20c59292982d25005436e628c06c410f73
fordai <48076288+fordai@users.noreply.github.com>
2019-04-11 12:10:49 +0800

    The basic architecture for this application. A simple user interface is implemented for development and testing.

---
+++ app/src/main/java/group/j/android/markdownald/ui/activity/MainActivity.java
@@ -0,0 +1,39 @@
+package group.j.android.markdownald.ui.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import group.j.android.markdownald.R;
+
+/**
+ * The home page of this app.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.create_note:
+                Intent intent = new Intent(this, NoteEditActivity.class);
+                startActivity(intent);
+            default:
+        }
+        return true;
+    }
+
+}
