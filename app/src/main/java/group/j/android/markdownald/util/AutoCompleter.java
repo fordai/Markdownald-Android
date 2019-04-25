2467a6e85f2e7df3d1c85ee3bc170553a5364d16
Donkequan <48150445+Donkequan@users.noreply.github.com>
2019-04-26 00:40:10 +0800

    Code Complipter update

--- app/src/main/java/group/j/android/markdownald/util/AutoCompleter.java
+++ app/src/main/java/group/j/android/markdownald/util/AutoCompleter.java
@@ -1,137 +1,60 @@
 package group.j.android.markdownald.util;
 
-import android.content.Context;
+
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import java.util.ArrayList;
+
 
 /**
  * Complete the markdown syntax automatically
  */
 public class AutoCompleter {
 
-    private ArrayList<String> al = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
     private EditText et;
-    private Context c;
-    private Spinner sp;
-    private int preLength = 0;
-
-    public ArrayAdapter<String> getAdapter() {
-        return adapter;
-    }
 
     public AutoCompleter(EditText et){
         this.et = et;
     }
 
-    public AutoCompleter(EditText et, Spinner sp, Context c){
-        this.et = et;
-        this.sp = sp;
-        this.c = c;
-    }
-
     public void run(){
-        initial();
         addMyTextListener();
-    }
-
-    private void addSpinner(){
-        adapter = new ArrayAdapter(c,android.R.layout.simple_spinner_dropdown_item,al);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp.setAdapter(adapter);
-        sp.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-//                addChar(et, parent.getSelectedItem().toString());
-            }
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
-        });
-    }
-
-    private void deletTextListener(){
-        et.addTextChangedListener(new TextWatcher() {
-            int length = 0;
-            int prelength = 0;
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                length = et.getText().length();
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                prelength = et.getText().length();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (length>prelength) {
-                    et.removeTextChangedListener(this);
-                    addMyTextListener();
-                }
-            }
-        });
     }
 
     private void addMyTextListener(){
         et.addTextChangedListener(new TextWatcher() {
-            String ss = "";
-            boolean run = false;
-            int length = 0;
-            int prelength = 0;
+            char add;
+            int point = 0;
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (et.getText().equals("")) {
-                    run = false;
-                } else {
-                    ss = et.getText().toString();
-                    if(al.contains(ss)){
-                        run = true;
-                    }else if(ss.startsWith("**")&&ss.charAt(1)!='*'&&!ss.endsWith("**")){
-                        run = true;
-                    }else if(ss.startsWith("*")&&ss.charAt(0)!='*'&&!ss.endsWith("*")){
-                        run = true;
-                    }
-                    length = et.getText().length();
+                if(count==1) {
+                    add = s.charAt(start);
+                    point = start+1;
                 }
             }
 
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                prelength = et.getText().length();
+
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (prelength>length){
+                if(add == '*'){
                     et.removeTextChangedListener(this);
-                    deletTextListener();
-                }
-                if (run) {
-                    et.removeTextChangedListener(this);
-                    et.append("*");
-                    et.setSelection(length / 2 + 1);
+                    s.insert(point, "*");
                     addMyTextListener();
+                    et.setSelection(point);
+                }else if(add == '#'){
+                    et.removeTextChangedListener(this);
+                    s.insert(point, "#");
+                    addMyTextListener();
+                    et.setSelection(point);
                 }
             }
         });
-    }
-
-    private void initial(){
-        al.add("***");
-        al.add("*");
-    }
-
-    public ArrayList<String> getAl(){
-        return al;
     }
 
 }
