fc7c14fd1c90da381c249f89506d428cc3871220
123biubiu <517278272@qq.com>
2019-05-13 21:52:58 +0800

    ui update for login and registration

--- app/src/main/java/group/j/android/markdownald/ui/activity/LoginActivity.java
+++ app/src/main/java/group/j/android/markdownald/ui/activity/LoginActivity.java
@@ -1,25 +1,42 @@
 package group.j.android.markdownald.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import group.j.android.markdownald.R;
 import group.j.android.markdownald.base.BaseActivity;
 public class LoginActivity extends BaseActivity {
-    private Button mBack2;
+    private Toolbar mToolbar;
+    private TextView toolbar_title;
+    private TextView text_tutorial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mBack2 = findViewById(R.id.button_back2);
-        mBack2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View iv) {
-                Intent backIntent = new Intent();
-                backIntent.setClass(LoginActivity.this,MainActivity.class);
-                startActivity(backIntent);
-            }
-        });
+        mToolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        toolbar_title = mToolbar.findViewById(R.id.toolbar_title);
+        toolbar_title.setText(getString(R.string.app_name));
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+
 }
