ed9308e156af75f6bf1c54984a8be0b9a813c6fc
fordai <48076288+fordai@users.noreply.github.com>
2019-05-16 18:26:52 +0800

    Add the license.

--- app/src/main/java/group/j/android/markdownald/ui/activity/AboutActivity.java
+++ app/src/main/java/group/j/android/markdownald/ui/activity/AboutActivity.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 
 import group.j.android.markdownald.R;
 import group.j.android.markdownald.base.BaseActivity;
+import group.j.android.markdownald.util.MarkdownRenderer;
 import group.j.android.markdownald.util.MarkdownSyntaxHighlighter;
 
 public class AboutActivity extends BaseActivity {
@@ -31,6 +32,24 @@ public class AboutActivity extends BaseActivity {
         }
         toolbar_title = mToolbar.findViewById(R.id.toolbar_title);
         toolbar_title.setText(getString(R.string.app_name));
+
+        // Load the tutorial
+        InputStream inputStream = null;
+        try {
+            inputStream = getAssets().open("About.txt");
+            int size = inputStream.available();
+            byte[] bytes = new byte[size];
+            inputStream.read(bytes);
+            inputStream.close();
+
+            String tutorial = new String(bytes);
+            text_about = findViewById(R.id.text_about);
+            text_about.setMovementMethod(ScrollingMovementMethod.getInstance());
+            MarkdownRenderer markdownRenderer = new MarkdownRenderer(this);
+            markdownRenderer.setMarkdown(text_about, tutorial);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
